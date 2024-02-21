6366e6498054d02e126c97971eeffa31f2e16ae4
Jiwon Choi <sjndlw1013@gmail.com>
2024-02-21 23:47:10 +0900

    점수 UI

--- Assets/Resources/Scripts/ScoreSystem.cs
+++ Assets/Resources/Scripts/ScoreSystem.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
     int score = 0;
     public int Score { get { return score; } }
     public void AddScore(int amount)
     {
         score += amount;
+    }
+
+    private void Start()
+    {
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        scoreText.SetText(score.ToString());
     }
 }
